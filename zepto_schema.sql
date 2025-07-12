a539ef247da1f1d4d7c96fad4618b740a1fbc3b6
sejalmahajan27 <sejalmahajan2005@gmail.com>
2025-07-12 21:59:20 +0530

    Create zepto_schema.sql

---
+++ zepto_schema.sql
@@ -0,0 +1,16 @@
+-- Drop the table if it already exists
+DROP TABLE IF EXISTS zepto;
+
+-- Create the Zepto inventory table
+CREATE TABLE zepto (
+    sku_id SERIAL PRIMARY KEY,
+    category VARCHAR(120),
+    name VARCHAR(150) NOT NULL,
+    mrp NUMERIC(8,2),
+    discountPercent NUMERIC(5,2),
+    availableQuantity INTEGER,
+    discountedSellingPrice NUMERIC(8,2),
+    weightInGms INTEGER,
+    outOfStock BOOLEAN,
+    quantity INTEGER
+);
